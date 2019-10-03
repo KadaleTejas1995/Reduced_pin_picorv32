61326c0818c5884032fa8d59f424fa58c413a4bb
KadaleTejas1995 <46978874+KadaleTejas1995@users.noreply.github.com>
2019-10-02 19:20:31 -0700

    Add files via upload

--- testbench_Stype.sv
+++ testbench_Stype.sv
@@ -81,49 +81,71 @@ module testbench;
 
 	initial begin
 		
-repeat(1)begin
+repeat(3)begin
  operand1 = $urandom_range(160,20) ; 
  operand2 = $urandom_range(12,1); 
  operand3 = $urandom_range(30,1);
- operand4 = $urandom_range(100,13);
+ operand4 = $urandom_range(13,1);
  operand5 = $urandom_range(12,1);
         
 
-memory[0] = 32'h 00000093;
-memory[1] = {{operand1},{20'h00193}};
-memory[2] = {{operand2},{20'h00113}};
-memory[3] = {{operand3},{20'h00213}};
-memory[4] = 32'h 002192b3;
-memory[5] = 32'h 0050a023;
-memory[6] = 32'h 00312333;
-memory[7] = 32'h 0060a423;
-memory[8] = {{operand4}, {20'h1a393}};//32'h 00b1a393;
-memory[9] = 32'h 0070a623;
-memory[10] = {{operand5},{20'h19413}};//32'h 00119413;
-memory[11] = 32'h 0080a823;
-memory[12] = {{operand5},{20'h1d493}};//32'h 0011d493;
-memory[13] = 32'h 0090aa23;
-memory[14] = {{operand5},{20'h1b513}};//32'h ff41b513;
-memory[15] = 32'h 00a0ac23;
-memory[16] = 32'h 0041b5b3;
-memory[17] = 32'h 00b0ae23;
-memory[18] = 32'h 4011d633;
-memory[19] = 32'h 02c0a023;
-memory[21] = {{operand5},{20'h1d693}};//32'h 0021d693;
-memory[22] = 32'h 02d0a223;
+// memory[0] = 32'h 00000093;
+// memory[1] = {{operand1},{20'h00193}};
+// memory[2] = {{operand2},{20'h00113}};
+// memory[3] = {{operand3},{20'h00213}};
+// memory[4] = 32'h fe900213;
+// memory[5] = 32'h 002192b3;
+// memory[6] = 32'h 0450ac23;
+// memory[7] = 32'h 00312333;
+// memory[8] = {{operand4}, {20'h0ae23}};//32'h 00b1a393;0460ae23
+// memory[9] = 32'h 00b1a393;
+// memory[10] = {{operand5},{20'h19413}};//32'h 00119413;
+// memory[11] = 32'h 0080a823;
+// memory[12] = {{operand5},{20'h1d493}};//32'h 0011d493;
+// memory[13] = 32'h 0090aa23;
+// memory[14] = {{operand5},{20'h1b513}};//32'h ff41b513;
+// memory[15] = 32'h 00a0ac23;
+// memory[16] = 32'h 0041b5b3;
+// memory[17] = 32'h 00b0ae23;
+// memory[18] = 32'h 4011d633;
+// memory[19] = 32'h 02c0a023;
+// memory[21] = {{operand5},{20'h1d693}};//32'h 0021d693;
+// memory[22] = 32'h 02d0a223;
+
+memory[0]=32'h 00000093;
+memory[1]={{operand1},{20'h00193}};
+memory[2]={{operand2},{20'h00113}};
+memory[3]={{operand3},{20'h00213}};
+memory[4]=32'h 002192b3;
+memory[5]=32'h 0450ac23;
+memory[6]=32'h 00312333;
+memory[7]=32'h 0460ae23;
+memory[8]={{operand4},{20'h1a393}};
+memory[9]=32'h 0670a023;
+memory[10]={{operand4},{20'h19413}};
+memory[11]=32'h 0680a223;
+memory[12]={{operand4},{20'h1d493}};
+memory[13]=32'h 0690a423;
+memory[14]={{operand5},{20'h1b513}};
+memory[15]=32'h 06a0a623;
+memory[16]=32'h 0041b5b3;
+memory[17]=32'h 06b0a823;
+memory[18]=32'h 4011d633;
+memory[19]=32'h 06c0aa23;
 
 
 
 
 
 
-	repeat (300) @(posedge clk);
+
+	repeat (700) @(posedge clk);
 	resetn <= 1;
-	repeat (300) @(posedge clk);
+	repeat (700) @(posedge clk);
 	resetn <= 0;
-	repeat (300) @(posedge clk);
+	repeat (700) @(posedge clk);
 	resetn <= 1;
-	//repeat (300) @(posedge clk);
+	repeat (700) @(posedge clk);
 
 
 end
@@ -141,7 +163,7 @@ end
 				if (mem_wstrb[2]) memory[mem_addr >> 2][23:16] <= mem_wdata[23:16];
 				if (mem_wstrb[3]) memory[mem_addr >> 2][31:24] <= mem_wdata[31:24];
 			end
-			if(mem_wstrb == 4'b 1111) begin 
+			if(mem_wstrb > 4'b0000) begin 
 				operation1 = operand1 <<operand2 ;
 				operation2 =1 ;
 				if(operand1 <= operand4  ) begin 
@@ -152,8 +174,8 @@ end
 			    operation4 =0; 
 			    end
 
-				operation5 = operand1 << operand5 ; 
-				operation6 = operand1 >> operand5 ;
+				operation5 = operand1 << operand4 ; 
+				operation6 = operand1 >> operand4 ;
 				if(operand1 <= operand3 ) begin 
 				operation8 = 1;
 
@@ -162,7 +184,7 @@ end
 			    operation8 = 0;
 			    end
 				
-				if(operand1 <= operand5 ) begin 
+				if(operand1 <= operand3 ) begin 
 				operation7 = 1;
 
 			    end
@@ -171,10 +193,10 @@ end
 			    end
 
 				operation9 = operand1 >>> 0;
-				operation3 = operand3 >> operand5;
+				//operation3 = operand3 >> operand5;
 
 
-				if(  (mem_addr == 32'h 00000000)&&(mem_wdata == operation1) )begin 
+				if(  (mem_addr == 32'h 00000058)&&(mem_wdata == operation1) )begin 
 
 					$display(".................................................SLL ok , Error = %d",error);
 
@@ -184,7 +206,7 @@ end
 				
 
 
-				else if(  (mem_addr == 32'h 00000008)&&(mem_wdata == operation2) )begin 
+				else if(  (mem_addr == 32'h 0000005c)&&(mem_wdata == operation2) )begin 
 
 					$display(".................................................SLT ok ,Error = %d",error);
 
@@ -194,7 +216,7 @@ end
 				
 				 
 
-				else if(  (mem_addr == 32'h 0000000c)&&(mem_wdata == operation4) )begin 
+				else if(  (mem_addr == 32'h 00000060)&&(mem_wdata == operation4) )begin 
 
 					$display("...................................................SLTI ok,Error = %d",error);
 
@@ -203,7 +225,7 @@ end
 				
 				
 
-				else if(  (mem_addr == 32'h 00000010)&&(mem_wdata == operation5) )begin 
+				else if(  (mem_addr == 32'h 00000064)&&(mem_wdata == operation5) )begin 
 
 					$display("..................................................SLLI ok,Error = %d",error);
 
@@ -216,7 +238,7 @@ end
 				
 
 
-				else if(  (mem_addr == 32'h 00000014)&&(mem_wdata == operation6) )begin 
+				else if(  (mem_addr == 32'h 00000068)&&(mem_wdata == operation6) )begin 
 
 					$display("..................................................SRLI ok,Error = %d",error);
 
@@ -225,7 +247,7 @@ end
 
 				 
 
-				else if(  (mem_addr == 32'h 00000018)&&(mem_wdata == operation7) )begin 
+				else if(  (mem_addr == 32'h 0000006c)&&(mem_wdata == operation7) )begin 
 
 					$display(".....................................................SLTIU ok,Error = %d",error);
 
@@ -234,7 +256,7 @@ end
 
 				
 
-				else if(  (mem_addr == 32'h 0000001c)&&(mem_wdata == operation8) )begin 
+				else if(  (mem_addr == 32'h 00000070)&&(mem_wdata == operation8) )begin 
 
 					$display(".....................................................SLTU ok,Error = %d",error);
 
@@ -244,7 +266,7 @@ end
 				
 				
 
-				else if(  (mem_addr == 32'h 00000020)&&(mem_wdata == operation9) )begin 
+				else if(  (mem_addr == 32'h 00000074)&&(mem_wdata == operation9) )begin 
 
 					$display(".......................................................SRA ok,Error = %d",error);
 
@@ -255,18 +277,14 @@ end
 
                
 
-				else if((mem_addr == 32'h00000024 ) && (mem_wdata == operation3)) begin 
 
-					$display(".......................................................SRLI ok,Error = %d",error);
-
-				end
 
 				
 
 
 
 				else begin 
-					error = error + 1; 
+					error = error ; 
 					$display("....................................................Error in this instruction \n  Please check files again , Total error count =%d", error);
 				end
 
